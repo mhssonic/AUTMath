d50ce7aaabf46ae111636713e69bc1cb6df91634
meidix <mahdihossieni@gmail.com>
2023-04-08 12:33:01 +0430

    added the assertResult function

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,14 @@
+
+
 public class Main {
+
+    public void assertResult(int testing, int expected) {
+        if (testing != expected) {
+            System.out.printf("\nAssertionError: %d != %d\n");
+        } else {
+            System.out.print('.');
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
