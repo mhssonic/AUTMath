06980ebe42a07aa33e394806be1391ff529c7c84
meidix <mahdihossieni@gmail.com>
2023-04-08 12:08:26 +0430

    Added AUTMath class

--- src/AUTMath.java
+++ src/AUTMath.java
@@ -2,4 +2,24 @@ public class AUTMath {
     public static int sum(int num1, int num2) {
         return num1 + num2;
     }
+
+    public static int subtract(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public static int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public static int divide(int num1, int num2) {
+        return num1 / num2;
+    }
+
+    public static int factorial(int number) {
+        return number * factorial(number -1);
+    }
+
+    public static int pow(int base, int power) {
+        return base * pow(base, power - 1);
+    }
 }
